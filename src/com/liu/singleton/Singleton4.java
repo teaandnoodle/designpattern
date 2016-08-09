4483426c997dbfd4590efac3f49673f145d08b67
teaandnoodle <teaandnoodle@163.com>
2016-08-09 23:13:13 +0800

    将Singleton4和Singleton5中的final域去掉，因为final代表常量字段，这样会在类加载时被载入常量池，虽说没有并发的错误，但是却没有延迟加载的作用了。

--- src/com/liu/singleton/Singleton4.java
+++ src/com/liu/singleton/Singleton4.java
@@ -3,7 +3,7 @@ package com.liu.singleton;
 public class Singleton4 {
 	private Singleton4(){}
 	
-	private static final Singleton4 instance = new Singleton4();
+	private static Singleton4 instance = new Singleton4();
 	
 	public static Singleton4 getInstance(){
 		return instance;
--- src/com/liu/singleton/Singleton5.java
+++ src/com/liu/singleton/Singleton5.java
@@ -4,7 +4,7 @@ public class Singleton5 {
 	private Singleton5(){}
 	
 	private static class Holder{
-		public static final Singleton5 instance = new Singleton5();
+		public static Singleton5 instance = new Singleton5();
 	}
 	
 	public static Singleton5 getInstance(){
